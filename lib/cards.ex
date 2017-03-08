6e626ded1dd9754a6753c05300986fe5a68bd0c2
Sarah Pimentel <sarah.pimentel@gmail.com>
2017-03-08 16:22:26 +0100

    Create method deal

--- lib/cards.ex
+++ lib/cards.ex
@@ -6,7 +6,6 @@ defmodule Cards do
     for suit <- suits, value <- values do
       "#{value} of #{suit}"
     end
-
   end
 
   def shuffle(deck) do
@@ -15,5 +14,9 @@ defmodule Cards do
 
   def contains?(deck, card) do
     Enum.member?(deck, card)
+  end
+
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
   end
 end
