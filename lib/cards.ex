c43377a589141ca9338a9c0d09d2d190eac5c014
Sarah Pimentel <sarah.pimentel@gmail.com>
2017-03-12 11:16:09 +0100

    create hand using pipe operator

--- lib/cards.ex
+++ lib/cards.ex
@@ -32,4 +32,16 @@ defmodule Cards do
     end
   end
 
+  def create_hand(hand_size) do
+    #without pipe operator
+    #deck = Cards.create_deck
+    #deck = Cards.shuffle(deck)
+    #hand = Cards.deal(deck, hand_size)
+
+    Cards.create_deck
+    |> Cards.shuffle
+    |> Cards.deal(hand_size)
+
+  end
+
 end
